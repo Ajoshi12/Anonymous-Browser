78b0142006c24bbf089c5f55c4bff4fe90fd48f5
NeelApte <neelapte@hotmail.com>
2019-07-12 22:00:59 -0700

    Update serverside.py

--- serverside.py
+++ serverside.py
@@ -1,20 +1,46 @@
-import socket
+from flask import Flask
+from flask import jsonify
+from flask import make_response
+from flask import request
 
-HOST, PORT = '', 8888
+app = Flask(__name__)
+k_v_store = {}
+@app.route("/key-value-store/<key>", methods = ["GET", "DELETE", "PUT"])
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print ('Serving HTTP on port %s ...' % PORT)
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print (request)
+def respond_request(key):
+    #handle GET requests
+    if request.method =="GET":
+        if key in k_v_store:
+            return make_response(jsonify(message = "Retrieved Successfully", doesExist = True, value=k_v_store[key]))
+        else:
+            return make_response(jsonify(message = "Retreived Unsuccesfully ", doesExist = False, value=k_v_store[key]))
 
-    http_response = """\
-HTTP/1.1 200 OK
-Hello, World!
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+
+    # handle PUT requests
+    elif request.method == "PUT":
+        if 'value' not in request.get_json():
+            return make_response(jsonify(message= "Error in PUT", error = "value is missing"), 400)
+        if len(key) > 50:
+            return make_response(jsonify(message="Error in PUT", error = "key too long"), 400)
+
+        value = request.get_json()['value']
+        if key in k_v_store:
+            k_v_store[key] = value
+            return make_response(jsonify(message = "updated Successfully", replaced = True), 200)
+        else:
+            k_v_store[key] = value
+            return make_response(jsonify(message = "added Successfully", replaced = False), 201)
+
+    # handle DELETE requests
+    elif request.method == "DELETE":
+        if key in k_v_store:
+            k_v_store.pop(key,None)
+            return make_response(jsonify(message = "Deleted Successfully", doesExist = True), 200)
+        else:
+            return make_response(jsonify(message = "Error in DELETE", doesExist = "False", error = "Key does not Exist"), 404)
+
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 8080)
+
