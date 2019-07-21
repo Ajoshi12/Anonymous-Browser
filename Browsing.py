570143e9ddc703f4cbfb81577ae62fab643523ad
RaghuC99 <raghavchaudhary99@gmail.com>
2019-07-21 15:56:51 -0700

    Fixed, but had to take out some functionality
    .

--- Browsing.py
+++ Browsing.py
@@ -1,19 +1,22 @@
 import csv
 import webbrowser
+import random
+import time
 def visit_sites(csv_file):
     with open(csv_file,'r') as open_file:
         csv_read = csv.reader(open_file)
-        row_count = sum(1 for row in csv_read)
-        counter = 2
+        next(csv_read)
+        next(csv_read)
         for row in csv_read:
-            url_to_visit  = row[0]
-            main_index = url.find('.com') +_4
-            main_url = url[:main_index]
-            print("The url we are visiting is")
-            print(main_url)
-            if (counter % 2 == 0):
-                webbrowser.open(main_url)
-            counter = counter + 1
+            skip_test_number = random.randint(1,10)
+            print(skip_test_number)
+            if(skip_test_number > 7):
+                print("Skipping this iteration!")
+                continue
+            url_to_visit = row[0]
+            print("Now visiting", url_to_visit)
+            time_to_sleep = random.randint(1,5)
+            time.sleep(time_to_sleep)
+            webbrowser.open(url_to_visit)
 
 visit_sites('test.csv')
-webbrowser.open('www.google.com')
