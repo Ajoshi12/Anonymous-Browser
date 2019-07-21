c25de589f846276059dcde7015f4ba0d75e6e6ab
chkulkar <50430919+chkulkar@users.noreply.github.com>
2019-07-21 13:11:42 -0700

    fixed adding users to new list & randomizing users

--- groupings.py
+++ groupings.py
@@ -1,33 +1,38 @@
-
 import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import Error
 import csv
 import random
 
 distinct_users = []
 try:
+    # connect to database and select User from db table
+    mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity') 
+    cursor = mydatabase.cursor()
+    sql_select_Query = "select User from dataTable"
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall() # runs the select statement to get all distinct users
+    for row in records:
+        distinct_users.append(row) # add each User to distinct_users list
+    cursor.close()
 
-# don't know if I need to reconnect again since aishu didn't close the connection
-mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
-
-cursor = mydatabase.cursor()
-sql_select_Query = "select distinct User from dataTable"
-records = cursor.fetchall() # runs the select statement to get all distinct users
-for row in records:
-       distinct_users.append(row)
-cursor.close()
-
-except Error as e :
+except Error as e:
     print ("Error while connecting to MySQL", e)
 
- # distinct_users list should have all the users rn
+finally:
+    #close database connection
+    if(mydatabase.is_connected()):
+        mydatabase.close()
+
+    # distinct_users list should have all the users now
  ###################################################################################
 
- # next step is to randomize
+# randomize users
 randomUsers = []
- while distinct_users is not []:
-     #  randomize the order
+pick = []
+while distinct_users != []:
+    #  pick a random user in list
     pick = random.choice(distinct_users)
+    print(randomUsers)
     randomUsers.append(pick) # adds the random pick to our random ordered list
     distinct_users.remove(pick) # remove the random pick from distinct_users
 
@@ -39,12 +44,8 @@ Random_list = [data_list[i:i+4] for i in range(0, len(data_list), 4)]
 for i in range(len(Random_list)):
     for j in range(len(Random_list[i])):
         creating_CSV(Random_list[i][j],"Randomized_History.csv")
-              
-SELECT mydatabase = mysql.connector.connect, host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity"
-FROM  csv.writer( SELECT user )
-as "cursor.execute" filename;
 
-def creating_CSV(ID,filename)
+def creating_CSV(ID, filename):
     try:
         mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
         cursor = mydatabase.cursor(prepared=True)
@@ -52,11 +53,12 @@ def creating_CSV(ID,filename)
         cursor.execute(sql_select_query, (ID, ))
         record = cursor.fetchall()
         with open(filename, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # writing into the csv file
         for row in records:
             csvwriter.writerow(row + ',')
         cursor.close()
+        mydatabase.close()
 
-    except Error as e :
+    except Error as e:
         print ("Error while in db when creating csv file", e)
