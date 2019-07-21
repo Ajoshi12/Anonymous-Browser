2ee80bdc4cf7da7a7367245618d78c9a6e4385ee
chkulkar <50430919+chkulkar@users.noreply.github.com>
2019-07-20 18:27:50 -0700

    added comments

--- db.py
+++ db.py
@@ -1,16 +1,20 @@
 
-import mysql.connector
-import csv
+import mysql.connector 
+import csv #import csv file
 
-
-mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
-cursor = mydatabase.cursor()
-csv_data = csv.reader(open('modify.csv'))
+#connect to database using db parameters
+mydb = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
+curs = mydb.cursor() #used to traverse database line by line
+csv_data = csv.reader(open('modify.csv')) #open csv file for data transfer
+next(csv_data) #skip header row of csv file
+#traverse through each row in csv file
 for row in csv_data:
-	cursor.execute('INSERT INTO dataTable(User, URL, Title) VALUES (%s, %s, %s)', row)
+	#insert csv data into database table
+	curs.execute('INSERT INTO dataTable(User, URL, Title) VALUES (%s, %s, %s)', row)
+curs.close()
+mydb.commit()
+mydb.close() #close cursor and database connection
 
-mydatabase.commit()
-cursor.close()
 
 
 
