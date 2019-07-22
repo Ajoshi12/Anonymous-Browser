5828c4127ee6c937f7abb656081908b0fb4001eb
NeelApte <neelapte@hotmail.com>
2019-07-21 23:44:32 -0700

    Update groupings.py

--- groupings.py
+++ groupings.py
@@ -3,6 +3,34 @@ from mysql.connector import Error
 import csv
 import random
 
+ef creating_CSV(Random_list, i,filename):
+    # this loop takes account of each member in the group
+    for j in range(len(Random_list[i])):
+        ID = Random_list[i][j]
+        try:
+            mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
+            cursor = mydatabase.cursor(prepared=True)
+            sql_select_query = """select * from dataTable where user = %s"""
+            cursor.execute(sql_select_query, (ID, ))
+            record = cursor.fetchall()
+            with open(filename, 'w', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # writing into the csv file
+            for row in records:
+                csvwriter.writerow(row + ',')
+            cursor.close()
+            mydatabase.close()
+
+        except Error as e:
+            print ("Error while in db when creating csv file", e)
+     # calling raghav's function
+     visit_sites("Random_history.csv")
+for i in range(len(Random_list)):
+    # now instead I'm calling the function for each group once
+    creating_CSV(Random_list, i,"Random_history.csv")
+
+
+
 distinct_users = []
 try:
     # connect to database and select User from db table
@@ -40,29 +68,3 @@ Random_list = [randomUsers[i:i+4] for i in range(0, len(randomUsers), 4)]
 #########################################################################################
 # this creates our groups
 # now we have to create a csv file for each group
-for i in range(len(Random_list)):
-    # now instead I'm calling the function for each group once
-    creating_CSV(Random_list, i,"Random_history.csv")
-
-def creating_CSV(Random_list, i,filename):
-    # this loop takes account of each member in the group
-    for j in range(len(Random_list[i])):
-        ID = Random_list[i][j]
-        try:
-            mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
-            cursor = mydatabase.cursor(prepared=True)
-            sql_select_query = """select * from dataTable where user = %s"""
-            cursor.execute(sql_select_query, (ID, ))
-            record = cursor.fetchall()
-            with open(filename, 'w', newline='') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            # writing into the csv file
-            for row in records:
-                csvwriter.writerow(row + ',')
-            cursor.close()
-            mydatabase.close()
-
-        except Error as e:
-            print ("Error while in db when creating csv file", e)
-     # calling raghav's function
-     visit_sites("Random_history.csv")
