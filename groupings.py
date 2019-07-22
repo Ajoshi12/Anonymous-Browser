9693cad7e96543680ceaf318b199ee396d10556c
Ajoshi12 <43459228+Ajoshi12@users.noreply.github.com>
2019-07-21 17:22:57 -0700

    improved logic

--- groupings.py
+++ groupings.py
@@ -6,7 +6,7 @@ import random
 distinct_users = []
 try:
     # connect to database and select User from db table
-    mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity') 
+    mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
     cursor = mydatabase.cursor()
     sql_select_Query = "select User from dataTable"
     cursor.execute(sql_select_Query)
@@ -38,26 +38,31 @@ while distinct_users != []:
 #########################After randomizing now we have to create groups####################
 Random_list = [randomUsers[i:i+4] for i in range(0, len(randomUsers), 4)]
 #########################################################################################
-# this creates our groups 
-# now we have to create a csv file for each group 
+# this creates our groups
+# now we have to create a csv file for each group
 for i in range(len(Random_list)):
+    # now instead I'm calling the function for each group once
+    creating_CSV(Random_list, i,"Random_history.csv")
+
+def creating_CSV(Random_list, i,filename):
+    # this loop takes account of each member in the group
     for j in range(len(Random_list[i])):
-        creating_CSV(Random_list[i][j],"Randomized_History.csv")
+        ID = Random_list[i][j]
+        try:
+            mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
+            cursor = mydatabase.cursor(prepared=True)
+            sql_select_query = """select * from dataTable where user = %s"""
+            cursor.execute(sql_select_query, (ID, ))
+            record = cursor.fetchall()
+            with open(filename, 'w', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # writing into the csv file
+            for row in records:
+                csvwriter.writerow(row + ',')
+            cursor.close()
+            mydatabase.close()
 
-def creating_CSV(ID, filename):
-    try:
-        mydatabase = mysql.connector.connect(host='localhost', user='crowdAnn', password='cmps115!', database='CrowdSourcedAnonymity')
-        cursor = mydatabase.cursor(prepared=True)
-        sql_select_query = """select * from dataTable where user = %s"""
-        cursor.execute(sql_select_query, (ID, ))
-        record = cursor.fetchall()
-        with open(filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        # writing into the csv file
-        for row in records:
-            csvwriter.writerow(row + ',')
-        cursor.close()
-        mydatabase.close()
-
-    except Error as e:
-        print ("Error while in db when creating csv file", e)
+        except Error as e:
+            print ("Error while in db when creating csv file", e)
+     # calling raghav's function
+     visit_sites("Random_history.csv")
