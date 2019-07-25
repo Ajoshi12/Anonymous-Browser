d5f2c6821a9605e4f3b71818ab8908430c24e48d
chkulkar <50430919+chkulkar@users.noreply.github.com>
2019-07-25 00:22:11 -0700

    updated

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ def server_recieving(filename):
             write_file.write(row)
             row = client_socket.recv(10000)
             if not row: break
+    #print message and close socket
     print("finished")
     client_socket.send("file transfer is complete")
     client_socket.close()
@@ -30,11 +31,11 @@ def server_recieving(filename):
 
 def server_sending(filename):
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    #connect to port
     server_address = ('0.0.0.0', 25565)
     print('connecting to %s port %s' % server_address)
     my_socket.connect(server_address)
-
+    #read csv file and sent to client
     with open(filename,'r') as open_file:
         csv_read = csv.reader(open_file)
         column_num = len(next(csv_read))
@@ -52,7 +53,7 @@ def server_sending(filename):
             print(row_send)
             row_send = row_send.encode("utf-8")
             my_socket.send(row_send)
-
+    #print message and close socket
     print("file has been sent")
     my_socket.shutdown(socket.SHUT_WR)
     my_socket.close
